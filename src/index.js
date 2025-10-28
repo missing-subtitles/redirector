bc626bbaf9e9be6ccd197e3ef9ea907d647a56f5
H3arn <root@h3a.moe>
2025-10-29 03:00:13 +0800

    fix: process es module correctly

--- src/index.js
+++ src/index.js
@@ -1,41 +1,33 @@
-export default {
-  async fetch(request, env) {
-    // pick deployment based on env variable
-    const ENV = env.MODE || 'main'
-  }
-}
-
 import configs from '../config.js'
 
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
+export default {
+  async fetch(request, env) {
+    const ENV = env.MODE || 'main'
+    try {
+      // get path after domain, trim spaces
+      const { pathname, search } = new URL(request.url)
+      const urlPath = (pathname + search).slice(1).trim()
 
-async function handleRequest(request) {
-  try {
-    // get path after domain, trim spaces
-    const { pathname, search } = new URL(request.url)
-    const urlPath = (pathname + search).slice(1).trim()
-    
-    // Path "/" 302 to github project home
-    if (!urlPath) return Response.redirect("https:/github.com/missing-subtitles/missing-subtitles", 302)
-    //return new Response('No URL provided', { status: 400 })
+      // Path "/" 302 to github project home
+      if (!urlPath) return Response.redirect("https:/github.com/missing-subtitles/missing-subtitles", 302)
+      //return new Response('No URL provided', { status: 400 })
 
-    // decode percent-encoded URL
-    const inputUrl = decodeURIComponent(urlPath)
+      // decode percent-encoded URL
+      const inputUrl = decodeURIComponent(urlPath)
 
-    // extract shortcode
-    const shortcode = extractShortcode(inputUrl)
-    if (!shortcode) return new Response('Invalid YouTube URL', { status: 400 })
+      // extract shortcode
+      const shortcode = extractShortcode(inputUrl)
+      if (!shortcode) return new Response('Invalid YouTube URL', { status: 400 })
 
-    // build target URL
-    const targetUrl = configs[ENV].buildTargetUrl(shortcode)
-    // console.info(targetUrl)
+      // build target URL
+      const targetUrl = configs[ENV].buildTargetUrl(shortcode)
+      // console.info(targetUrl)
 
-    // redirect
-    return Response.redirect(targetUrl, 302)
-  } catch (err) {
-    return new Response('Error: ' + err.message, { status: 500 })
+      // redirect
+      return Response.redirect(targetUrl, 302)
+    } catch (err) {
+      return new Response('Internal Server Error: ' + err.message, { status: 500 })
+    }
   }
 }
 
